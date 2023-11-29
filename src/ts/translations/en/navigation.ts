676c84e10834401121e6988f90d3877478534fa1
Alexander Bakiev <alexander.bakiev@dataart.com>
2023-11-30 00:51:57 +0100

    Update capitalization in navigation translation file
    Capitalization of words in the 'navigation.ts' translation file for English has been updated for clarity and consistency. Also, some words in Russian were replaced with their English equivalents for better understanding. No functionality changes were made.

--- src/ts/translations/en/navigation.ts
+++ src/ts/translations/en/navigation.ts
@@ -3,35 +3,35 @@ export default `
 § sidebar.switch.person: Employee
 § sidebar.buttons.settings: Settings
 § sidebar.buttons.print: Print
-§ sidebar.filters.all: all time
-§ sidebar.filters.year: year
-§ sidebar.filters.halfYear: half year
-§ sidebar.filters.month: month
-§ sidebar.filters.week: week
-§ sidebar.team.total: Common info
+§ sidebar.filters.all: All Time
+§ sidebar.filters.year: Year
+§ sidebar.filters.halfYear: Half Year
+§ sidebar.filters.month: Month
+§ sidebar.filters.week: Week
+§ sidebar.team.total: Common Info
 § sidebar.team.scope: Features
 § sidebar.team.author: Employees
-§ sidebar.team.type: Task types
-§ sidebar.team.pr: Pull requests
-§ sidebar.team.day: By day
-§ sidebar.team.week: By week
-§ sidebar.team.month: By month
+§ sidebar.team.type: Task Types
+§ sidebar.team.pr: Pull Requests
+§ sidebar.team.day: By Day
+§ sidebar.team.week: By Week
+§ sidebar.team.month: By Month
 § sidebar.team.tree: Files
-§ sidebar.team.hours: Расписание
-§ sidebar.team.commits: All commits
-§ sidebar.team.changes: All changes
-§ sidebar.team.words: Popular words
-§ sidebar.team.top: Викторина
+§ sidebar.team.hours: Schedule
+§ sidebar.team.commits: All Commits
+§ sidebar.team.changes: All Changes
+§ sidebar.team.words: Popular Words
+§ sidebar.team.top: Quiz
 § sidebar.team.settings: Settings
-§ sidebar.person.total: Common info
-§ sidebar.person.money: Work cost
+§ sidebar.person.total: Common Info
+§ sidebar.person.money: Work Cost
 § sidebar.person.speed: Speed
-§ sidebar.person.day: By day
-§ sidebar.person.week: By week
-§ sidebar.person.month: By month
-§ sidebar.person.hours: Расписание
-§ sidebar.person.commits: All commits
-§ sidebar.person.changes: All changes
-§ sidebar.person.words: Popular words
+§ sidebar.person.day: By Day
+§ sidebar.person.week: By Week
+§ sidebar.person.month: By Month
+§ sidebar.person.hours: Schedule
+§ sidebar.person.commits: All Commits
+§ sidebar.person.changes: All Changes
+§ sidebar.person.words: Popular Words
 § sidebar.person.settings: Settings
 `;
