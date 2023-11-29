d8fb33c42504ec12e9d9a57b6f61469950f5d69a
Alexander Bakiev <alexander.bakiev@dataart.com>
2023-11-30 00:45:29 +0100

    Update English translations in 'common.ts'
    The commit translates various sections in 'common.ts' from Russian to English. The updated fields include UIKit elements, notifications, filters and descriptions, enhancing the user interface clarity for English-speaking users.

--- src/ts/translations/en/common.ts
+++ src/ts/translations/en/common.ts
@@ -1,28 +1,28 @@
 export default `
-§ uiKit.console: Копировать
-§ uiKit.dataLoader.page: Страница
-§ uiKit.dataLoader.size: Отображается по
-§ uiKit.dataLoader.from: из
-§ uiKit.dataLoader.all: Показать все
-§ uiKit.hoursChart.work: стандартное рабочее время (будни, с 07:00 до 20:00)
-§ uiKit.hoursChart.weekend: выходные дни или время до/после рабочего дня
-§ uiKit.hoursChart.days: суммарное количество коммитов за все время в конкретный день и час
-§ uiKit.page.remove: Удалить
-§ uiKit.races.go: Поехали
-§ uiKit.nothingFound.common.title: Нет или недостаточно данных для отображения
-§ uiKit.nothingFound.common.description: Система обработает больше данных, если коммиты будут подписаны в формате [Git commit message convention|https://www.conventionalcommits.org/en/v1.0.0/]. Шаблон:
-§ uiKit.nothingFound.common.console: Номер_задачи тип(фича): пояснение
-§ uiKit.nothingFound.common.example: Например:
-§ uiKit.nothingFound.staff.title: Нет данных для этого сотрудника
+§ uiKit.console: Copy
+§ uiKit.dataLoader.page: Page
+§ uiKit.dataLoader.size: Displaying by
+§ uiKit.dataLoader.from: from
+§ uiKit.dataLoader.all: Show all
+§ uiKit.hoursChart.work: Standard working hours (weekdays, from 07:00 to 20:00)
+§ uiKit.hoursChart.weekend: Weekend or time before/after working hours
+§ uiKit.hoursChart.days: Total number of commits for a specific day and hour over time
+§ uiKit.page.remove: Remove
+§ uiKit.races.go: Go
+§ uiKit.nothingFound.common.title: No Data or Insufficient Data for Display
+§ uiKit.nothingFound.common.description: The system will process more data if commits are signed in the [Git commit message convention|https://www.conventionalcommits.org/en/v1.0.0/] format. Template:
+§ uiKit.nothingFound.common.console: Task_Number type(feature): explanation
+§ uiKit.nothingFound.common.example: For example:
+§ uiKit.nothingFound.staff.title: No data available for this employee
 § uiKit.nothingFound.staff.description1:
-Он вносил правки не каждый рабочий день и получил статус  «Помошник».
-Работой сотрудников с таким статусом по данному проекту можно пренебречь, т.к. его влад на общем фоне незначителен.
+They made changes not every working day and have the "Assistant" status.
+The work of employees with this status on this project can be disregarded because their contribution is insignificant compared to others.
 
 § uiKit.nothingFound.staff.description2:
-Поэтому система не рассчитывает для него ряд показателей.
-Если это ошибка и данного сотрудника нужно рассчитать как обычного, перейдите в раздел «Настройки» и измените его тип.
+Therefore, the system does not calculate certain metrics for them.
+If this is an error, and this employee should be treated as a regular employee, go to the "Settings" section and change their type.
 
-§ common.filters: Фильтры
-§ common.notifications.save: Изменения сохранены
-§ common.notifications.setting: Настройки сохранены
+§ common.filters: Filters
+§ common.notifications.save: Changes saved
+§ common.notifications.setting: Settings saved
 `;
