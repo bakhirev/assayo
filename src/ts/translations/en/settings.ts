52e28bdeef38c34139dfbd58502db059d9619fe6
Alexander Bakiev <alexander.bakiev@dataart.com>
2023-11-30 00:43:10 +0100

    Translate settings texts in en/settings.ts
    The commit contains translations from Russian to English for all text related to settings in en/settings.ts file. This includes titles, instructions, notifications, and button captions for different sections related to page, link, user, and common settings.

--- src/ts/translations/en/settings.ts
+++ src/ts/translations/en/settings.ts
@@ -1,27 +1,27 @@
 export default `
-§ page.settings.document.title: Настройки отображения
-§ page.settings.document.name: Заголовок страницы
-§ page.settings.document.language: Язык интерфейса
-§ page.settings.links.title: Префиксы ссылок
-§ page.settings.links.task: Для номеров задач
-§ page.settings.links.pr: Для PR
-§ page.settings.user.title: Индивидуальные настройки
-§ page.settings.user.notFound: Индивидуальных настроек нет. Данные по всем сотрудникам вычисляются по общим параметрам.
-§ page.settings.user.subTitle: Дополнение к трудовому договору №$1
-§ page.settings.user.from: Дата начала действия
-§ page.settings.mailmap: Настройки .mailmap
-§ page.settings.common.title: Общие данные по зарплате
-§ page.settings.common.type.title: Тип работы над проектом
-§ page.settings.common.type.full: Полная занятость
-§ page.settings.common.type.part: Проектная работа
-§ page.settings.common.salary: Зарплата в месяц
+§ page.settings.document.title: Display Settings
+§ page.settings.document.name: Page Title
+§ page.settings.document.language: Interface Language
+§ page.settings.links.title: Link Prefixes
+§ page.settings.links.task: For Task Numbers
+§ page.settings.links.pr: For PR
+§ page.settings.user.title: Individual Settings
+§ page.settings.user.notFound: No individual settings found. Data for all employees are calculated based on common parameters.
+§ page.settings.user.subTitle: Addendum to Employment Contract No. $1
+§ page.settings.user.from: Effective Date
+§ page.settings.mailmap: .mailmap Settings
+§ page.settings.common.title: Common Salary Data
+§ page.settings.common.type.title: Type of Project Engagement
+§ page.settings.common.type.full: Full-time Employment
+§ page.settings.common.type.part: Project Work
+§ page.settings.common.salary: Monthly Salary
 § page.settings.common.currency: Currency
-§ page.settings.common.workDaysInYear: Количество рабочих дней в году
-§ page.settings.common.vacationDaysInYear: Количество дней отпуска в год
-§ page.settings.common.workDaysInWeek: Рабочие дни
+§ page.settings.common.workDaysInYear: Number of Working Days in a Year
+§ page.settings.common.vacationDaysInYear: Number of Vacation Days in a Year
+§ page.settings.common.workDaysInWeek: Working Days
 § page.settings.form.save: Save
 § page.settings.form.cancel: Cancel
 § page.settings.form.remove: Remove
-§ page.settings.form.addEmployee: Добавить сотрудника
-§ page.settings.form.addContract: Добавить трудовой договор
+§ page.settings.form.addEmployee: Add Employee
+§ page.settings.form.addContract: Add Employment Contract
 `;
