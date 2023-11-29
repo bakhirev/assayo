d5fd127f42d8252ffd9b2531da6f58c71f84acf7
Alexander Bakiev <alexander.bakiev@dataart.com>
2023-11-30 00:41:07 +0100

    Translate achievements texts in en/achievements.ts
    The commit contains translations from Russian to English for all text related to achievements in en/achievements.ts file. This includes titles and descriptions for various categories capturing different project and employee performance metrics.

--- src/ts/translations/en/achievements.ts
+++ src/ts/translations/en/achievements.ts
@@ -1,94 +1,94 @@
 export default `
-§ achievements.commitsAfter1500.title: Сова
-§ achievements.commitsAfter1500.description: 70% коммитов после 15:00
-§ achievements.commitsBefore1500.title: Ранняя пташка
-§ achievements.commitsBefore1500.description: 70% коммитов до обеда
-§ achievements.workEveryTime.title: Раб божий
-§ achievements.workEveryTime.description: есть коммит на каждый час суток
-§ achievements.workNotWork.title: Стрельба холостыми
-§ achievements.workNotWork.description: коммиты есть, а закрытых задач нет
-§ achievements.userNotWork.title: Залётный
-§ achievements.userNotWork.description: это не его основной проект
-§ achievements.userIsDied.title: Мёртвая душа
-§ achievements.userIsDied.description: работал, но уволился
-§ achievements.lessTasks.title: Зашел и вышел
-§ achievements.lessTasks.description: меньше всего закрытых задач
-§ achievements.moreTasks.title: Батя грит малаца
-§ achievements.moreTasks.description: больше всего закрытых задач
-§ achievements.everyMessageLong.title: Мастер красноречия
-§ achievements.everyMessageLong.description: стабильно самые длинные подписи коммитов
-§ achievements.everyMessageShort.title: Болтун находка для шпиона
-§ achievements.everyMessageShort.description: стабильно, самые короткие подписи коммитов
-§ achievements.shortestName.title: Размер не главное
-§ achievements.shortestName.description: самое короткое имя
-§ achievements.longestName.title: Азим Азиз Иль Ам Кадир Имран II
-§ achievements.longestName.description: самое длинное имя
-§ achievements.moreCommits.title: Мастер бекапов
-§ achievements.moreCommits.description: больше всего коммитов
-§ achievements.lessCommits.title: Редко но метко
-§ achievements.lessCommits.description: меньше всего коммитов
-§ achievements.oneCommitOneTask.title: Точно в цель
-§ achievements.oneCommitOneTask.description: в среднем один коммит на задачу
-§ achievements.moreLazyDays.title: Мысленно я с вами
-§ achievements.moreLazyDays.description: больше всего дней без коммитов
-§ achievements.lessLazyDays.title: Папа Карло
-§ achievements.lessLazyDays.description: меньше всего дней без коммитов
-§ achievements.zeroLazyDays.title: Ни единого разрыва
-§ achievements.zeroLazyDays.description: ни одного дня без коммитов
-§ achievements.moreWorkDays.title: Ценный работник
-§ achievements.moreWorkDays.description: больше всего рабочих дней
-§ achievements.moreScopes.title: Стартапер
-§ achievements.moreScopes.description: сделал больше всего фичей
-§ achievements.lessScopes.title: Щегол
-§ achievements.lessScopes.description: сделал меньше всего фичей
-§ achievements.moreDaysForTask.title: Улитка на склоне
-§ achievements.moreDaysForTask.description: работа по задачам идёт медленнее чем у остальных
-§ achievements.more2DaysForTask.title: Cо слоу
-§ achievements.more2DaysForTask.description: больше двух дней на задачу
-§ achievements.moreDaysInProject.title: Старожил
-§ achievements.moreDaysInProject.description: больше всего дней на проекте
-§ achievements.lessDaysInProject.title: А это кто?
-§ achievements.lessDaysInProject.description: меньше всего дней на проекте
-§ achievements.more90DaysInProject.title: Добро пожаловать
-§ achievements.more90DaysInProject.description: не уволили на испытательном
-§ achievements.lessDaysForTask.title: Скорострел
-§ achievements.lessDaysForTask.description: одна задача занимает меньше дня
-§ achievements.adam.title: Адам
-§ achievements.adam.description: первый стабильный сотрудник на проекте
-§ achievements.more666DaysInProject.title: Чёрт
-§ achievements.more666DaysInProject.description: отработал 666 дней на проекте
-§ achievements.more777DaysInProject.title: Азино 3 топора
-§ achievements.more777DaysInProject.description: отработал 777 дней на проекте
-§ achievements.moreRefactoring.title: Выпускающий редактор
-§ achievements.moreRefactoring.description: сделал больше всех меток «рефакторинг»
-§ achievements.longestMessage.title: А разговоров то было...
-§ achievements.longestMessage.description: самая длинная подпись коммита за все время
-§ achievements.moreTasksInDay.title: Спиди-гонщик
-§ achievements.moreTasksInDay.description: рекорд по количеству закрытых задач в день
-§ achievements.hasCommitFrom0to7.title: Ночной дозор
-§ achievements.hasCommitFrom0to7.description: есть коммит на каждый час ночи
-§ achievements.noCommitOnDay.title: Технический перерыв
-§ achievements.noCommitOnDay.description: есть определенный час и день в рабочее время в который никогда не комитит
-§ achievements.hasCommitEveryTime.title: Умер на работе
-§ achievements.hasCommitEveryTime.description: есть коммит на час каждого дня (включая выходные)
-§ achievements.commitsAfter1800.title: Делу время
-§ achievements.commitsAfter1800.description: нет ни одного коммита после 18:00
-§ achievements.more1488DaysInProject.title: им. Максима Марцинкевича
-§ achievements.more1488DaysInProject.description: отработал 1488 дней на проекте
-§ achievements.taskNumber300.title: Знаком с трактористом
-§ achievements.taskNumber300.description: первый взял в работу задачу с номером 300
-§ achievements.moreFix.title: Bug hunter
-§ achievements.moreFix.description: больше всего закрытых багов
-§ achievements.lessWorkDays.title: Дальше без меня
-§ achievements.lessWorkDays.description: меньше всего рабочих дней
-§ achievements.moreCreateCode.title: Созидатель
-§ achievements.moreCreateCode.description: склонен больше остальных добавлять код
-§ achievements.moreRemoveCode.title: Разрушитель
-§ achievements.moreRemoveCode.description: склонен больше остальных удалять код
-§ achievements.moreChangeCode.title: Реформатор
-§ achievements.moreChangeCode.description: склонен больше остальных изменять код
-§ achievements.moreStyle.title: Полиция моды
-§ achievements.moreStyle.description: склонен больше остальных изменять CSS
-§ achievements.moreOnHoliday.title: Нет жизни
-§ achievements.moreOnHoliday.description: относительно много коммитов в нерабочее время
+§ achievements.commitsAfter1500.title: Night Owl
+§ achievements.commitsAfter1500.description: 70% of commits after 3:00 PM
+§ achievements.commitsBefore1500.title: Early Bird
+§ achievements.commitsBefore1500.description: 70% of commits before noon
+§ achievements.workEveryTime.title: Dedicated Worker
+§ achievements.workEveryTime.description: Has a commit for every hour of the day
+§ achievements.workNotWork.title: Shooting Blanks
+§ achievements.workNotWork.description: Has commits but no closed tasks
+§ achievements.userNotWork.title: Outsider
+§ achievements.userNotWork.description: This is not their primary project
+§ achievements.userIsDied.title: Dead Soul
+§ achievements.userIsDied.description: Worked but quit
+§ achievements.lessTasks.title: In and Out
+§ achievements.lessTasks.description: The fewest closed tasks
+§ achievements.moreTasks.title: Prolific Worker
+§ achievements.moreTasks.description: The most closed tasks
+§ achievements.everyMessageLong.title: Master Orator
+§ achievements.everyMessageLong.description: Consistently has the longest commit messages
+§ achievements.everyMessageShort.title: Chatterbox
+§ achievements.everyMessageShort.description: Consistently has the shortest commit messages
+§ achievements.shortestName.title: Size Doesn't Matter
+§ achievements.shortestName.description: The shortest name
+§ achievements.longestName.title: Azim Aziz Il Am Kadir Imran II
+§ achievements.longestName.description: The longest name
+§ achievements.moreCommits.title: Backup Master
+§ achievements.moreCommits.description: The most commits
+§ achievements.lessCommits.title: Rare but Precise
+§ achievements.lessCommits.description: The fewest commits
+§ achievements.oneCommitOneTask.title: Bullseye
+§ achievements.oneCommitOneTask.description: On average, one commit per task
+§ achievements.moreLazyDays.title: Mentally with You
+§ achievements.moreLazyDays.description: The most days without commits
+§ achievements.lessLazyDays.title: Papa Carlo
+§ achievements.lessLazyDays.description: The fewest days without commits
+§ achievements.zeroLazyDays.title: No Breaks
+§ achievements.zeroLazyDays.description: Not a single day without commits
+§ achievements.moreWorkDays.title: Valuable Employee
+§ achievements.moreWorkDays.description: The most working days
+§ achievements.moreScopes.title: Startup Enthusiast
+§ achievements.moreScopes.description: Implemented the most features
+§ achievements.lessScopes.title: Dandy
+§ achievements.lessScopes.description: Implemented the fewest features
+§ achievements.moreDaysForTask.title: Slow and Steady
+§ achievements.moreDaysForTask.description: Progress on tasks is slower than others
+§ achievements.more2DaysForTask.title: Extra Slow
+§ achievements.more2DaysForTask.description: More than two days per task
+§ achievements.moreDaysInProject.title: Seasoned Veteran
+§ achievements.moreDaysInProject.description: The most days on the project
+§ achievements.lessDaysInProject.title: Who's This?
+§ achievements.lessDaysInProject.description: The fewest days on the project
+§ achievements.more90DaysInProject.title: Welcome Aboard
+§ achievements.more90DaysInProject.description: Not fired during the probation period
+§ achievements.lessDaysForTask.title: Speed Demon
+§ achievements.lessDaysForTask.description: Completes a task in less than a day
+§ achievements.adam.title: Adam
+§ achievements.adam.description: The first stable employee on the project
+§ achievements.more666DaysInProject.title: Devilish
+§ achievements.more666DaysInProject.description: Worked for 666 days on the project
+§ achievements.more777DaysInProject.title: Azino 3 Axes
+§ achievements.more777DaysInProject.description: Worked for 777 days on the project
+§ achievements.moreRefactoring.title: Refactoring Guru
+§ achievements.moreRefactoring.description: Created the most "refactoring" tags
+§ achievements.longestMessage.title: And the Talk Goes On...
+§ achievements.longestMessage.description: The longest commit message of all time
+§ achievements.moreTasksInDay.title: Speed Racer
+§ achievements.moreTasksInDay.description: Set a record for the most closed tasks in a day
+§ achievements.hasCommitFrom0to7.title: Night Watchman
+§ achievements.hasCommitFrom0to7.description: Has a commit for every hour of the night
+§ achievements.noCommitOnDay.title: Technical Break
+§ achievements.noCommitOnDay.description: Has a specific hour and day during working hours when no commits are made
+§ achievements.hasCommitEveryTime.title: Died at Work
+§ achievements.hasCommitEveryTime.description: Has a commit for every hour of each day (including weekends)
+§ achievements.commitsAfter1800.title: Time for Action
+§ achievements.commitsAfter1800.description: No commits after 6:00 PM
+§ achievements.more1488DaysInProject.title: Maxim Marcinkevich Memorial
+§ achievements.more1488DaysInProject.description: Worked for 1488 days on the project
+§ achievements.taskNumber300.title: Tractor Enthusiast
+§ achievements.taskNumber300.description: Took the first task with number 300
+§ achievements.moreFix.title: Bug Hunter
+§ achievements.moreFix.description: Closed the most bugs
+§ achievements.lessWorkDays.title: Moving On
+§ achievements.lessWorkDays.description: The fewest working days
+§ achievements.moreCreateCode.title: Creator
+§ achievements.moreCreateCode.description: More inclined than others to add code
+§ achievements.moreRemoveCode.title: Destroyer
+§ achievements.moreRemoveCode.description: More inclined than others to remove code
+§ achievements.moreChangeCode.title: Reformer
+§ achievements.moreChangeCode.description: More inclined than others to change code
+§ achievements.moreStyle.title: Fashion Police
+§ achievements.moreStyle.description: More inclined than others to change CSS
+§ achievements.moreOnHoliday.title: No Life
+§ achievements.moreOnHoliday.description: Relatively many commits during non-working hours
 `;
