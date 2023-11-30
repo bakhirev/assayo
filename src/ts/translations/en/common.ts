30ad64933a5782c3d2ffcce8348f672f89b3cf05
Alexander Bakiev <alexander.bakiev@dataart.com>
2023-11-30 01:18:23 +0100

    Refine translations in English common.ts file
    This commit contains text updates to the English common.ts file to improve clarity and readability. Specific changes include refinement to phrases for more natural language and eliminating redundancies. The aim is to overall enhance the user experience.

--- src/ts/translations/en/common.ts
+++ src/ts/translations/en/common.ts
@@ -5,22 +5,22 @@ export default `
 § uiKit.dataLoader.from: from
 § uiKit.dataLoader.all: Show all
 § uiKit.hoursChart.work: Standard working hours (weekdays, from 07:00 to 20:00)
-§ uiKit.hoursChart.weekend: Weekend or time before/after working hours
-§ uiKit.hoursChart.days: Total number of commits for a specific day and hour over time
+§ uiKit.hoursChart.weekend: Weekends or time before/after working day
+§ uiKit.hoursChart.days: Total number of commits at any specific day and hour over time
 § uiKit.page.remove: Remove
-§ uiKit.races.go: Go
-§ uiKit.nothingFound.common.title: No Data or Insufficient Data for Display
-§ uiKit.nothingFound.common.description: The system will process more data if commits are signed in the [Git commit message convention|https://www.conventionalcommits.org/en/v1.0.0/] format. Template:
+§ uiKit.races.go: Let's go
+§ uiKit.nothingFound.common.title: No or Insufficient Data for Display
+§ uiKit.nothingFound.common.description: The system will process more data if commits are formatted according to the [Git commit message convention|https://www.conventionalcommits.org/en/v1.0.0/]. Template:
 § uiKit.nothingFound.common.console: Task_Number type(feature): explanation
 § uiKit.nothingFound.common.example: For example:
-§ uiKit.nothingFound.staff.title: No data available for this employee
+§ uiKit.nothingFound.staff.title: No data for this employee
 § uiKit.nothingFound.staff.description1:
-They made changes not every working day and have the "Assistant" status.
-The work of employees with this status on this project can be disregarded because their contribution is insignificant compared to others.
+They made edits not every working day and received the "Assistant" status.
+The work of employees with such status on this project can be neglected as their contribution is insignificant in the general context.
 
 § uiKit.nothingFound.staff.description2:
 Therefore, the system does not calculate certain metrics for them.
-If this is an error, and this employee should be treated as a regular employee, go to the "Settings" section and change their type.
+If this is an error and the employee should be treated as regular, go to the "Settings" section and change their type.
 
 § common.filters: Filters
 § common.notifications.save: Changes saved
