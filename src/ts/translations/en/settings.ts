c9cd6b0cbd47e17dae79dcae0dd791c3d88229b9
Alexander Bakiev <alexander.bakiev@dataart.com>
2023-11-30 01:53:40 +0100

    Refine wording in English localization file
    This commit makes several tweaks to the src/ts/translations/en/settings.ts file to streamline language and improve clarity. Changes include use of simpler, clearer language and removal of unnecessary words, enhancing usability and user satisfaction.

--- src/ts/translations/en/settings.ts
+++ src/ts/translations/en/settings.ts
@@ -6,19 +6,19 @@ export default `
 § page.settings.links.task: For Task Numbers
 § page.settings.links.pr: For PR
 § page.settings.user.title: Individual Settings
-§ page.settings.user.notFound: No individual settings found. Data for all employees are calculated based on common parameters.
-§ page.settings.user.subTitle: Addendum to Employment Contract No. $1
-§ page.settings.user.from: Effective Date
+§ page.settings.user.notFound: No individual settings. Data for all employees are calculated based on common parameters.
+§ page.settings.user.subTitle: Addendum to employment contract №$1
+§ page.settings.user.from: Start date
 § page.settings.mailmap: .mailmap Settings
-§ page.settings.common.title: Common Salary Data
-§ page.settings.common.type.title: Type of Project Engagement
-§ page.settings.common.type.full: Full-time Employment
+§ page.settings.common.title: General Salary Data
+§ page.settings.common.type.title: Type of Work on the Project
+§ page.settings.common.type.full: Full-time
 § page.settings.common.type.part: Project Work
 § page.settings.common.salary: Monthly Salary
 § page.settings.common.currency: Currency
 § page.settings.common.workDaysInYear: Number of Working Days in a Year
 § page.settings.common.vacationDaysInYear: Number of Vacation Days in a Year
-§ page.settings.common.workDaysInWeek: Working Days
+§ page.settings.common.workDaysInWeek: Workdays
 § page.settings.form.save: Save
 § page.settings.form.cancel: Cancel
 § page.settings.form.remove: Remove
