31a29c1356e45389e17b473efdb47db86614ce88
Alexander Bakiev <alexander.bakiev@dataart.com>
2023-11-30 01:06:26 +0100

    Update language and phrasing in achievements.ts
    The commit changes the titles and descriptions of achievements within the achievements.ts file to provide more clear and creative descriptions. It refines the language and style of the texts to enhance user readability and engagement.

--- src/ts/translations/en/achievements.ts
+++ src/ts/translations/en/achievements.ts
@@ -3,92 +3,92 @@ export default `
 § achievements.commitsAfter1500.description: 70% of commits after 3:00 PM
 § achievements.commitsBefore1500.title: Early Bird
 § achievements.commitsBefore1500.description: 70% of commits before noon
-§ achievements.workEveryTime.title: Dedicated Worker
-§ achievements.workEveryTime.description: Has a commit for every hour of the day
+§ achievements.workEveryTime.title: Devoted Worker
+§ achievements.workEveryTime.description: a commit for every hour of the day
 § achievements.workNotWork.title: Shooting Blanks
-§ achievements.workNotWork.description: Has commits but no closed tasks
-§ achievements.userNotWork.title: Outsider
-§ achievements.userNotWork.description: This is not their primary project
+§ achievements.workNotWork.description: commits are there, but no tasks are closed
+§ achievements.userNotWork.title: Fly-By
+§ achievements.userNotWork.description: this is not their main project
 § achievements.userIsDied.title: Dead Soul
-§ achievements.userIsDied.description: Worked but quit
-§ achievements.lessTasks.title: In and Out
-§ achievements.lessTasks.description: The fewest closed tasks
-§ achievements.moreTasks.title: Prolific Worker
-§ achievements.moreTasks.description: The most closed tasks
-§ achievements.everyMessageLong.title: Master Orator
-§ achievements.everyMessageLong.description: Consistently has the longest commit messages
-§ achievements.everyMessageShort.title: Chatterbox
-§ achievements.everyMessageShort.description: Consistently has the shortest commit messages
+§ achievements.userIsDied.description: worked, but resigned
+§ achievements.lessTasks.title: Came and Went
+§ achievements.lessTasks.description: least closed tasks
+§ achievements.moreTasks.title: Father Says Well Done
+§ achievements.moreTasks.description: most closed tasks
+§ achievements.everyMessageLong.title: Master of Eloquence
+§ achievements.everyMessageLong.description: consistently the longest commit messages
+§ achievements.everyMessageShort.title: Talkative, A Spy's Dream
+§ achievements.everyMessageShort.description: consistently, the shortest commit messages
 § achievements.shortestName.title: Size Doesn't Matter
-§ achievements.shortestName.description: The shortest name
+§ achievements.shortestName.description: shortest name
 § achievements.longestName.title: Azim Aziz Il Am Kadir Imran II
-§ achievements.longestName.description: The longest name
+§ achievements.longestName.description: longest name
 § achievements.moreCommits.title: Backup Master
-§ achievements.moreCommits.description: The most commits
+§ achievements.moreCommits.description: most commits
 § achievements.lessCommits.title: Rare but Precise
-§ achievements.lessCommits.description: The fewest commits
-§ achievements.oneCommitOneTask.title: Bullseye
-§ achievements.oneCommitOneTask.description: On average, one commit per task
-§ achievements.moreLazyDays.title: Mentally with You
-§ achievements.moreLazyDays.description: The most days without commits
+§ achievements.lessCommits.description: least commits
+§ achievements.oneCommitOneTask.title: Right on Target
+§ achievements.oneCommitOneTask.description: on average one commit per task
+§ achievements.moreLazyDays.title: With You in Spirit
+§ achievements.moreLazyDays.description: most days without commits
 § achievements.lessLazyDays.title: Papa Carlo
-§ achievements.lessLazyDays.description: The fewest days without commits
-§ achievements.zeroLazyDays.title: No Breaks
-§ achievements.zeroLazyDays.description: Not a single day without commits
+§ achievements.lessLazyDays.description: least days without commits
+§ achievements.zeroLazyDays.title: Not a Single Break
+§ achievements.zeroLazyDays.description: not a single day without commits
 § achievements.moreWorkDays.title: Valuable Employee
-§ achievements.moreWorkDays.description: The most working days
-§ achievements.moreScopes.title: Startup Enthusiast
-§ achievements.moreScopes.description: Implemented the most features
+§ achievements.moreWorkDays.description: most working days
+§ achievements.moreScopes.title: Startuper
+§ achievements.moreScopes.description: completed the most features
 § achievements.lessScopes.title: Dandy
-§ achievements.lessScopes.description: Implemented the fewest features
-§ achievements.moreDaysForTask.title: Slow and Steady
-§ achievements.moreDaysForTask.description: Progress on tasks is slower than others
-§ achievements.more2DaysForTask.title: Extra Slow
-§ achievements.more2DaysForTask.description: More than two days per task
-§ achievements.moreDaysInProject.title: Seasoned Veteran
-§ achievements.moreDaysInProject.description: The most days on the project
-§ achievements.lessDaysInProject.title: Who's This?
-§ achievements.lessDaysInProject.description: The fewest days on the project
+§ achievements.lessScopes.description: completed the fewest features
+§ achievements.moreDaysForTask.title: Snail on the Slope
+§ achievements.moreDaysForTask.description: tasks take longer than others
+§ achievements.more2DaysForTask.title: Slow and Steady
+§ achievements.more2DaysForTask.description: more than two days per task
+§ achievements.moreDaysInProject.title: Old-Timer
+§ achievements.moreDaysInProject.description: most days in the project
+§ achievements.lessDaysInProject.title: And Who Is This?
+§ achievements.lessDaysInProject.description: least days in the project
 § achievements.more90DaysInProject.title: Welcome Aboard
-§ achievements.more90DaysInProject.description: Not fired during the probation period
-§ achievements.lessDaysForTask.title: Speed Demon
-§ achievements.lessDaysForTask.description: Completes a task in less than a day
+§ achievements.more90DaysInProject.description: not fired during the probation period
+§ achievements.lessDaysForTask.title: Quick Draw
+§ achievements.lessDaysForTask.description: a task takes less than a day
 § achievements.adam.title: Adam
-§ achievements.adam.description: The first stable employee on the project
-§ achievements.more666DaysInProject.title: Devilish
-§ achievements.more666DaysInProject.description: Worked for 666 days on the project
-§ achievements.more777DaysInProject.title: Azino 3 Axes
-§ achievements.more777DaysInProject.description: Worked for 777 days on the project
-§ achievements.moreRefactoring.title: Refactoring Guru
-§ achievements.moreRefactoring.description: Created the most "refactoring" tags
-§ achievements.longestMessage.title: And the Talk Goes On...
-§ achievements.longestMessage.description: The longest commit message of all time
+§ achievements.adam.description: the first stable employee on the project
+§ achievements.more666DaysInProject.title: Devil
+§ achievements.more666DaysInProject.description: worked 666 days on the project
+§ achievements.more777DaysInProject.title: Azino Three Axes
+§ achievements.more777DaysInProject.description: worked 777 days on the project
+§ achievements.moreRefactoring.title: Executive Editor
+§ achievements.moreRefactoring.description: made the most 'refactoring' tags
+§ achievements.longestMessage.title: So Many Conversations...
+§ achievements.longestMessage.description: the longest commit message of all time
 § achievements.moreTasksInDay.title: Speed Racer
-§ achievements.moreTasksInDay.description: Set a record for the most closed tasks in a day
-§ achievements.hasCommitFrom0to7.title: Night Watchman
-§ achievements.hasCommitFrom0to7.description: Has a commit for every hour of the night
+§ achievements.moreTasksInDay.description: record for the number of tasks closed in a day
+§ achievements.hasCommitFrom0to7.title: Night Watch
+§ achievements.hasCommitFrom0to7.description: a commit for every hour of the night
 § achievements.noCommitOnDay.title: Technical Break
-§ achievements.noCommitOnDay.description: Has a specific hour and day during working hours when no commits are made
+§ achievements.noCommitOnDay.description: there is a certain hour and day in working time when never commits
 § achievements.hasCommitEveryTime.title: Died at Work
-§ achievements.hasCommitEveryTime.description: Has a commit for every hour of each day (including weekends)
-§ achievements.commitsAfter1800.title: Time for Action
-§ achievements.commitsAfter1800.description: No commits after 6:00 PM
-§ achievements.more1488DaysInProject.title: Maxim Marcinkevich Memorial
-§ achievements.more1488DaysInProject.description: Worked for 1488 days on the project
-§ achievements.taskNumber300.title: Tractor Enthusiast
-§ achievements.taskNumber300.description: Took the first task with number 300
+§ achievements.hasCommitEveryTime.description: a commit for every hour of each day (including weekends)
+§ achievements.commitsAfter1800.title: Business Time
+§ achievements.commitsAfter1800.description: no commits after 6:00 PM
+§ achievements.more1488DaysInProject.title: Named After Maxim Martsinkevich
+§ achievements.more1488DaysInProject.description: worked 1488 days on the project
+§ achievements.taskNumber300.title: Knows the Tractor Driver
+§ achievements.taskNumber300.description: first to take on task number 300
 § achievements.moreFix.title: Bug Hunter
-§ achievements.moreFix.description: Closed the most bugs
-§ achievements.lessWorkDays.title: Moving On
-§ achievements.lessWorkDays.description: The fewest working days
+§ achievements.moreFix.description: most closed bugs
+§ achievements.lessWorkDays.title: Count Me Out
+§ achievements.lessWorkDays.description: least working days
 § achievements.moreCreateCode.title: Creator
-§ achievements.moreCreateCode.description: More inclined than others to add code
+§ achievements.moreCreateCode.description: tends to add code more than others
 § achievements.moreRemoveCode.title: Destroyer
-§ achievements.moreRemoveCode.description: More inclined than others to remove code
+§ achievements.moreRemoveCode.description: tends to remove code more than others
 § achievements.moreChangeCode.title: Reformer
-§ achievements.moreChangeCode.description: More inclined than others to change code
+§ achievements.moreChangeCode.description: tends to change code more than others
 § achievements.moreStyle.title: Fashion Police
-§ achievements.moreStyle.description: More inclined than others to change CSS
+§ achievements.moreStyle.description: tends to change CSS more than others
 § achievements.moreOnHoliday.title: No Life
-§ achievements.moreOnHoliday.description: Relatively many commits during non-working hours
+§ achievements.moreOnHoliday.description: relatively many commits in non-working hours
 `;
